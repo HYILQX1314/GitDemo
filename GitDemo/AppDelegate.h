a41ccd93de8528fb9fc06e605a7122f5d3a5215f
HYILQX1314 <1131356109@qq.com>
2014-04-01 11:14:36 +0800

    Initial Commit

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by netsoft on 14-4-1.
+//  Copyright (c) 2014年 netsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
